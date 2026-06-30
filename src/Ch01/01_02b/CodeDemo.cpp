0982c611fae9a01dce472850e1c78b6a46efb9f0
gokulj2025ece-bit <gokul.j.2025.ece@rajalakshmi.edu.in>
2026-06-30 11:13:40 +0000

    Code

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -2,11 +2,25 @@
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
 
-#include<iostream>
+/*#include<iostream>
 
 int main() {
     std::cout << "Hi Hello!!" << std::endl ;
 
     std::cout<< std::endl << std::endl;
+    return 0;
+} */
+
+//Another way
+// #include<cstdio>
+// int main() {
+//     printf("Madara Uchiha\n");
+//     return 0;
+// }
+
+#include<iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "HI!!!" << std::endl;
     return 0;
 }
