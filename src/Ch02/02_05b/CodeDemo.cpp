9497cec1ee2396873254c220059855ce7fbdc4b7
gokulj2025ece-bit <gokul.j.2025.ece@rajalakshmi.edu.in>
2026-07-01 11:57:21 +0000

    Code

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -3,8 +3,21 @@
 // Using Variables, by Eduardo Corpeño 
 
 #include <iostream>
-
+int a, b = 5;
 int main(){
+    bool myflag;
+    a = 7;
+    myflag = false;
+    std::cout <<"a = " << a << std::endl;
+    std::cout <<"b = " << b << std::endl;
+    std::cout <<"flag = " << myflag << std::endl; 
+    myflag = true;
+    std::cout <<"a+b = " << a+b << std::endl;
+    std::cout <<"b-a = " << b-a << std::endl;
+    std::cout <<"flag = " << myflag << std::endl;
+    unsigned int m;
+    m = b - a;
+    std::cout << "b - a (unsigned) = " << m << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
