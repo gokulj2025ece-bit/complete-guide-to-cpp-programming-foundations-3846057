b24dff482a22cbe44989566c0cfe3cab97497a51
gokulj2025ece-bit <gokul.j.2025.ece@rajalakshmi.edu.in>
2026-07-03 13:52:27 +0000

    Code

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -7,8 +7,20 @@
 
 enum class character_role {protagonist, antagonist, sidekick, npc};
 
+struct game_character{
+    std::string name;
+    int level;
+    character_role role;
+};
+
 int main(){
     
+    game_character Nagappan;
+    Nagappan.name = "Momo";
+    Nagappan.level = 99;
+    Nagappan.role = character_role::protagonist;
+
+    std::cout << "Name of the character is " << Nagappan.name << ", level is " << Nagappan.level << ", role is " << (int) Nagappan.role << "." << std::endl;
     std::cout << std::endl << std::endl;
     return 0;
 }
