8fb4bc1dc8281c7cf5c8761cde72b41c1d5611b1
gokulj2025ece-bit <gokul.j.2025.ece@rajalakshmi.edu.in>
2026-07-10 15:05:40 +0000

    Code

--- src/Ch04/04_06b/CodeDemo.cpp
+++ src/Ch04/04_06b/CodeDemo.cpp
@@ -4,9 +4,17 @@
 
 #include <iostream>
 #include <string>
+#include<vector>
 
 int main(){
-    
+    std::vector<std::string> checkpoints = {"Start","Forest","Castle"};
+
+    checkpoints.push_back("Cave");
+    checkpoints.push_back("Finish");
+    std::cout << "The no. of checkpoints = " << checkpoints.size() << std::endl;
+    std::cout << checkpoints[2] << std::endl;
+    checkpoints[2] = "Black Castle";
+    std::cout << "The changed name is : " << checkpoints[2] << std::endl;
     std::cout << std::endl << std::endl;
     return 0;
 }
