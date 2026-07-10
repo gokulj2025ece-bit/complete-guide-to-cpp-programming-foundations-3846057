10dc99bc975a9b272abcb5a9f9160efbfe1c7f12
gokulj2025ece-bit <gokul.j.2025.ece@rajalakshmi.edu.in>
2026-07-10 15:22:45 +0000

    Code

--- src/Ch04/04_07b/CodeDemo.cpp
+++ src/Ch04/04_07b/CodeDemo.cpp
@@ -7,7 +7,18 @@
 #include <complex>
 
 int main(){
-    
+
+    std::vector<std::complex<double>> points;
+    points.push_back(std::complex<double>(3.5,2.5));
+    points.push_back(std::complex<double>(0.5,6.7));
+    points.push_back(std::complex<double>(-9.0,-7.7));
+    points.push_back(std::complex<double>(1.1,-4.5));
+
+    std::cout << points.begin()->real() <<std::endl;
+    std::cout << next(points.begin(),1)->imag() <<std::endl;
+    std::cout << prev(points.end(),2)->real() <<std::endl;
+    std::cout << (points.end()-1)->imag() <<std::endl;
+
     std::cout << std::endl << std::endl;
     return 0;
 }
